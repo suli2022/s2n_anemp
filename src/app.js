687485f5102e31453747f3a70b0036527277ccbd
Sallai András <joska@zold.and>
2022-09-21 09:15:35 +0200

    Dolgozók lekérése kész. Van nyomógomb.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
-var url = 'http://localhost:3000/employees';
-fetch(url)
-.then( result => result.json())
-.then( data => {
-    console.log(data);
+const dowButton = document.querySelector("#dowButton");
+
+dowButton.addEventListener('click', () => {
+    console.log('működik');
 });
+
+var url = 'http://localhost:3000/employees';
+
+fetch(url)
+.then( response => response.json())
+.then( result => console.log(result[0].name  ))
+.catch(error => {
+    console.log('Hiba! A lekérdezés sikertelen!');
+    console.log(error);
+});
+
+
