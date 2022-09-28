01d018f1af9386e8c0c45ae638b7cb7564dfe061
Sallai András <joska@zold.and>
2022-09-28 11:52:26 +0200

    Letöltés gomb törölve

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-const dowButton = document.querySelector("#dowButton");
 const addButton = document.querySelector("#addButton");
 const empTable = document.querySelector("#empTable");
 const empName = document.querySelector("#name");
@@ -13,10 +12,6 @@ const host = 'http://localhost:3000';
    getEmployees();
    
 })();
-
-dowButton.addEventListener('click', () => {    
-    
-});
 
 function getEmployees() {
     let endpoint = 'employees';
